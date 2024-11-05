57c7712210c438f02391aa277f02d4c17ebed78f
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-05 13:17:53 +0100

    Created inner class implementation of iterator

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -24,4 +24,19 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
     }
+
+    public class IteratorImpl implements Iterator<T>{
+        private int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            return (current > 0 && current < collection.length) ? true : false; 
+        }
+
+        @Override
+        public T next() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'next'");
+        }
+    }
 }
