c33c0fe84d5bad326e47ff3b599a195d3eb9d453
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-08 11:48:02 +0100

    Implement sorting methods for MonthSorterNested

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,8 +1,7 @@
 package it.unibo.nestedenum;
 
+
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
@@ -11,11 +10,21 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(final String o1,final String o2) {
+                return Integer.compare(Month.fromString(o1).getDays(), Month.fromString(o2).getDays()); 
+            }
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+            @Override
+            public int compare(final String o1,final String o2) {
+                return Month.fromString(o1).compareTo(Month.fromString(o2));
+            }
+        };
     }
 }
